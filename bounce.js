fa56f18279b3898165d0dbab14f1653523e9d5d9
David Song <songdavid98@gmail.com>
2016-04-21 12:56:00 -0400

    edited

--- bounce.js
+++ bounce.js
@@ -4,24 +4,15 @@ var context = canvas.getContext("2d");
 var startbutton = document.getElementById("start");
 var stopbutton = document.getElementById("stop");
 
-
-var rectW = 50;
-var rectH = 20;
-var rectX = 100;
-var rectY = 50;
-var xdir = 1;
-var ydir = 1;
 var frameid = 0;
 
 var run = function(e) {
     context.clearRect(0, 0, canvas.width, canvas.height);  
     console.log("clear");             
     for (object in allObjects) {
-	console.log(object);
+	object.move;
+	object.drawRect;
     };
-    first.move();
-    first.drawRect();
-    //drawRect();
     frameid = window.requestAnimationFrame(run);
 };
 
@@ -35,8 +26,6 @@ var stop = function() {
 }
 startbutton.addEventListener('click', start);
 stopbutton.addEventListener('click', stop);
-
-var allObjects = [1, 2, 3];
 
 var makeBouncers = function() {
     var rectW = 50;
@@ -70,3 +59,7 @@ var makeBouncers = function() {
 };
 
 var first = makeBouncers();
+var second = makeBouncers();
+var third = makeBouncers();
+var fourth = makeBouncers();
+var allObjects = [first, second, third, fourth];
