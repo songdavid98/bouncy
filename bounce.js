220dc1c55f343bf1126f04930377143f95f4c566
TiffL <lei.tiffany@gmail.com>
2016-04-21 17:56:58 -0400

    bounce

--- bounce.js
+++ bounce.js
@@ -1,8 +1,10 @@
-console.log("hello, david");
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 var startbutton = document.getElementById("start");
 var stopbutton = document.getElementById("stop");
+var clearbutton = document.getElementById("clear");
+var addbutton = document.getElementById("add");
+var running = false;
 
 var frameid = 0;
 
@@ -19,45 +21,46 @@ var run = function(e) {
 	//move();
 	//drawRect();
     //};
-    //first.move();
-    //first.drawRect();
     frameid = window.requestAnimationFrame(run);
 };
 
 var start = function() {
+    running = true;
     stop();
     console.log("started");
     run();
-}
+};
 var stop = function() {
+    running = false;
     window.cancelAnimationFrame(frameid);
-}
+};
+
 startbutton.addEventListener('click', start);
 stopbutton.addEventListener('click', stop);
 
 var makeBouncers = function() {
-    var rectW = 50;
-    var rectH = 20;
-    var rectX = Math.floor((Math.random() * canvas.width));
-    var rectY = Math.floor((Math.random() * canvas.height));
+    var radius = 15;
+    var posX = Math.floor((Math.random() * (canvas.width-2*radius) + radius));
+    var posY = Math.floor((Math.random() * (canvas.height-2*radius) + radius));
     var xdir = 1;
     var ydir = 1;
 
     var move = function() {
-	if (rectX <= 0 || rectX >= canvas.width - rectW) {
+	if (posX - radius <= 0 || posX >= canvas.width - radius) {
 	    xdir *= -1;
 	}
-	if (rectY <= 0 || rectY >= canvas.height - rectH) {
+	if (posY - radius <= 0 || posY >= canvas.height - radius) {
 	    ydir *= -1;
 	}
 	
-	rectX += xdir;
-	rectY += ydir;
+	posX += xdir;
+	posY += ydir;
     };
     
     var drawRect = function(e) { 
 	context.beginPath();
-	context.fillRect(rectX, rectY, rectW, rectH);
+	context.arc(posX, posY, radius, 0, 2*Math.PI);
+	context.fill();
     };
     
     return {
@@ -66,8 +69,28 @@ var makeBouncers = function() {
     };
 };
 
+
 var first = makeBouncers();
-var second = makeBouncers();
-var third = makeBouncers();
-var fourth = makeBouncers();
-var allObjects = [first, second, third, fourth];
+var allObjects = [first];
+
+var addBall = function(){
+    var newBall = makeBouncers();
+    allObjects.push(newBall);
+    //console.log("added");
+    if (running){
+	start();
+    }
+    else if (!running){
+	newBall.drawRect();
+    }
+};
+
+
+var clear = function(){
+    stop();
+    context.clearRect(0, 0, canvas.width, canvas.height);  
+    allObjects = [first];
+};
+
+addbutton.addEventListener('click',addBall);
+clearbutton.addEventListener("click",clear);
